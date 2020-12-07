e09737f521b9f089e92a06e88c04765c59d62a14
TroyFluegge <troy@hashicorp.com>
2020-12-07 16:35:19 -0600

    first commit

---
+++ main.tf
@@ -0,0 +1,72 @@
+locals {
+   common_tags = {
+    Name = "${var.prefix}-networking"
+    Env   = var.environment
+    }
+}
+
+resource "aws_vpc" "app_vpc" {
+  cidr_block           = var.address_space
+  enable_dns_hostnames = true
+
+  tags = local.common_tags
+}
+
+resource "aws_subnet" "app_subnet" {
+  vpc_id     = aws_vpc.app_vpc.id
+  cidr_block = var.subnet_prefix
+
+  tags = local.common_tags
+}
+
+resource "aws_security_group" "webapp_security_group" {
+  name = "${var.prefix}-security-group"
+
+  vpc_id = aws_vpc.app_vpc.id
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port       = 0
+    to_port         = 0
+    protocol        = "-1"
+    cidr_blocks     = ["0.0.0.0/0"]
+    prefix_list_ids = []
+  }
+
+  tags = local.common_tags
+}
+
+resource "aws_internet_gateway" "app_gateway" {
+  vpc_id = aws_vpc.app_vpc.id
+
+  tags = local.common_tags
+}
+
+resource "aws_route_table" "app_route_table" {
+  vpc_id = aws_vpc.app_vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.app_gateway.id
+  }
+}
